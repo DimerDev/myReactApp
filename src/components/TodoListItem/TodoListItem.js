115ba5971aaf1701723fa4603cae1d3c3d52446a
DimerDev <erasovwork@gmail.com>
2021-11-05 10:47:12 +0200

    add important in TodoListItem

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -6,35 +6,52 @@ const TodoListItem = ({ text }) => {
       done : false,
       important: false 
    });
-   
-   let classNames = 'list-group-item justify-content-center pb-4';
+         
+   let classNames = 'list-group-item justify-content-center';
    const complete = ' list-group-item-success';
+   const important = ' font-weight-bold text-danger';
    if (item.done) classNames += complete;
+   if (item.important) classNames += important;
 
    const handleDone = () => {
       if (item.done){
          setDone({
-            done:false,
+            done: false,
             important: false
          });
       }else {
          setDone({
-            done:true,
+            done: true,
             important: false
+         });
+      }
+   };
+   
+   const hadleImportant = (event) => {
+      console.log(event.target);
+      if (item.important){
+         setDone({
+            done: false,
+            important: false
+         });
+      }else {
+         setDone({
+            done: false,
+            important: true
          });
       }
    };
 
    return(
-      <span>
-         <li className={classNames} onClick={handleDone}>{text}
+      <span className={classNames} >
+         <span onClick={handleDone}>{text}</span>
             <button type="button" className="btn float-right">
                <i className="fas fa-trash fa-1x"></i>
             </button>
-            <button type="button" className="btn float-right ">
+            <button type="button" className="btn float-right" onClick={hadleImportant}>
                <i className="fas fa-exclamation-triangle"></i>
             </button>
-         </li>
+         
       </span>
    );
 };
